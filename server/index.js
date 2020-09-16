5bcc13f24e9af0c55afc3fc2e0ef60d796fe3b8e
michael5522 <michaelchang.kuo@gmail.com>
2020-09-16 03:25:49 +0000

    user can view details of product back end

--- server/index.js
+++ server/index.js
@@ -21,8 +21,36 @@ app.get('/api/health-check', (req, res, next) => {
 
 app.get('/api/products', (req, res, next) => {
   db.query('select "productId", "name", "price", "image", "shortDescription" from "products"')
-    .then(result => res.json(result.rows))
+    .then(result => {
+      // console.log(result);
+      res.json(result.rows);
+    })
+
     .catch(err => next(err));
+});
+
+app.get('/api/products/:productId', (req, res, next) => {
+  const input = req.params.productId;
+  // console.log('what you typed for productID',req.params.productId);
+  if (input <= 0) {
+    return res.status(400).send({
+      error: ` productID:${input} must be a postive number`
+    });
+  } else {
+    const params = [input];
+    const sql2 = 'select * from "products" where "productId" =$1';
+    db.query(sql2, params)
+      .then(result => {
+        if (!result.rows[0]) {
+          const message = `error: product id ${input} not found`;
+          return next(new ClientError(message, 404));
+        }
+
+        // console.log(result.rows);
+        res.json(result.rows[0]);
+      })
+      .catch(err => next(err));
+  }
 });
 
 app.use('/api', (req, res, next) => {
